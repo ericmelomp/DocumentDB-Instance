40a24a18b45569c7276c6aff46f103a17a27d7a2
Eric Melo <ericmelomp@gmail.com>
2022-07-19 18:58:09 -0300

    correção de informações erradas

--- resources.tf
+++ resources.tf
@@ -1,5 +1,5 @@
-resource "aws_docdb_cluster" "db_instance" {
-  for_each = var.document_db
+resource "aws_docdb_cluster_instance" "db_instance" {
+  for_each = var.document_db_instance
 
   cluster_identifier = each.value["cluster_identifier"]
   identifier         = each.value["identifier"]
