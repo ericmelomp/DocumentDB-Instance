a7bed9ac047672326d467d852178b461a6b29459
Eric Melo <ericmelomp@gmail.com>
2022-07-20 11:12:32 -0300

    Criação dos Shields Stats no arquivo README e adicionando comentário nos códigos

--- variables.tf
+++ variables.tf
@@ -1,10 +1,13 @@
 variable "document_db_instance" {
+  description = "Defining the types of arguments in the map of object"
   type = map(object({
 
     identifier         = string
     cluster_identifier = string
     instance_class     = string
   }))
+#For creating the instance, you need to set the cluster_identifier.
+#If you are creating both at once, use the meta-argument "depends_on".
   default = {
     "instance-docdb" = {
       cluster_identifier = "docdb-cluster-demo"
@@ -13,3 +16,4 @@ variable "document_db_instance" {
     }
   }
 }
+#You can create the tf.vars file instead of using "default" for the values
